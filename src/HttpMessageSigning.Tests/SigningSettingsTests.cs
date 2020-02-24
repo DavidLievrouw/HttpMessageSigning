fe6872e4c9487cea32847e346375fd93fd93258b
David Lievrouw <david.lievrouw@recomatics.com>
2020-02-24 14:38:15 +0100

    Add missing tests for SigningSettings validation.

---
+++ src/HttpMessageSigning.Tests/SigningSettingsTests.cs
@@ -0,0 +1,65 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning {
+    public class SigningSettingsTests {
+        private readonly SigningSettings _sut;
+
+        public SigningSettingsTests() {
+            _sut = new SigningSettings {
+                Expires = TimeSpan.FromMinutes(5),
+                KeyId = new KeyId(SignatureAlgorithm.HMAC, HashAlgorithm.SHA256, "abc123"),
+                Headers = new[] {
+                    HeaderName.PredefinedHeaderNames.RequestTarget,
+                    HeaderName.PredefinedHeaderNames.Date,
+                    HeaderName.PredefinedHeaderNames.Expires,
+                    new HeaderName("dalion_app_id")
+                }
+            };
+        }
+
+        public class Validate : SigningSettingsTests {
+            [Fact]
+            public void WhenKeyIdIsNull_ThrowsValidationException() {
+                _sut.KeyId = null;
+                Action act = () => _sut.Validate();
+                act.Should().Throw<HttpMessageSigningValidationException>();
+            }
+            
+            [Fact]
+            public void WhenHeadersIsNull_ThrowsValidationException() {
+                _sut.Headers = null;
+                Action act = () => _sut.Validate();
+                act.Should().Throw<HttpMessageSigningValidationException>();
+            }
+            
+            [Fact]
+            public void WhenHeadersIsEmpty_DoesNotThrow() {
+                _sut.Headers = Array.Empty<HeaderName>();
+                Action act = () => _sut.Validate();
+                act.Should().NotThrow();
+            }
+            
+            [Fact]
+            public void WhenExpiresIsNegative_ThrowsValidationException() {
+                _sut.Expires = TimeSpan.FromSeconds(-1);
+                Action act = () => _sut.Validate();
+                act.Should().Throw<HttpMessageSigningValidationException>();
+            }
+            
+            [Fact]
+            public void WhenExpiresIsZero_ThrowsValidationException() {
+                _sut.Expires = TimeSpan.Zero;
+                Action act = () => _sut.Validate();
+                act.Should().Throw<HttpMessageSigningValidationException>();
+            }
+            
+            [Fact]
+            public void WhenEverythingIsValid_DoesNotThrow() {
+                Action act = () => _sut.Validate();
+                act.Should().NotThrow();
+            }
+        }
+    }
+}
