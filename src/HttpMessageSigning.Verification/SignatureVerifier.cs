e1d59be6ce313561c5b7159597058f8554a80b36
David Lievrouw <david.lievrouw@recomatics.com>
2020-02-26 09:52:13 +0100

    SR: Add comment that indicates what should be checked.

--- src/HttpMessageSigning.Verification/SignatureVerifier.cs
+++ src/HttpMessageSigning.Verification/SignatureVerifier.cs
@@ -4,6 +4,15 @@ namespace Dalion.HttpMessageSigning.Verification {
     internal class SignatureVerifier : ISignatureVerifier {
         public Task VerifySignature(Signature signature, Client client) {
             // ToDo: Implement verification here, and throw when invalid
+            
+            // SignatureAlgorithm and HashAlgorithm from KeyId should match Algorithm parameter
+            // Throw on unsupported algorithm
+            // Throw when RSA or HMAC and (created) is specified
+            // Header specified in settings, and missing in request: Throw!
+            // Created in the future: Throw!
+            // Expires in the past: Throw!
+            // Signature should match
+            
             return Task.CompletedTask;
         }
     }
