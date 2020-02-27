8275af2a5c1a980429d01f9a25351a25ab0681a8
David Lievrouw <david.lievrouw@recomatics.com>
2020-02-27 14:04:06 +0100

    Update Publish script.

--- Publish.cmd
+++ Publish.cmd
@@ -9,3 +9,6 @@ RD /S /Q %DISTDIR%
 
 dotnet restore %SRCDIR%\%PRODUCT%.sln
 dotnet publish %SRCDIR%\%PRODUCT%\%PRODUCT%.csproj --no-restore --configuration Release --framework netstandard2.0 /p:IsPublishing="true" /p:PublishDir="%DISTDIR%\%PRODUCT%\netstandard2.0" /p:PackagePublishDir="%DISTDIR%\NuGetPackages"
+dotnet publish %SRCDIR%\%PRODUCT%.Signing\%PRODUCT%.Signing.csproj --no-restore --configuration Release --framework netstandard2.0 /p:IsPublishing="true" /p:PublishDir="%DISTDIR%\%PRODUCT%.Signing\netstandard2.0" /p:PackagePublishDir="%DISTDIR%\NuGetPackages"
+dotnet publish %SRCDIR%\%PRODUCT%.Verification\%PRODUCT%.Verification.csproj --no-restore --configuration Release --framework netstandard2.0 /p:IsPublishing="true" /p:PublishDir="%DISTDIR%\%PRODUCT%.Verification\netstandard2.0" /p:PackagePublishDir="%DISTDIR%\NuGetPackages"
+dotnet publish %SRCDIR%\%PRODUCT%.Verification.AuthenticationScheme\%PRODUCT%.Verification.AuthenticationScheme.csproj --no-restore --configuration Release --framework netstandard2.0 /p:IsPublishing="true" /p:PublishDir="%DISTDIR%\%PRODUCT%.Verification.AuthenticationScheme\netstandard2.0" /p:PackagePublishDir="%DISTDIR%\NuGetPackages"
