2df12bd6624ab1174f1ff7ba87cf133503648289
David Lievrouw <david.lievrouw@recomatics.com>
2020-02-27 15:08:52 +0100

    Cover creation and expiration time verification tasks by tests.

---
+++ src/HttpMessageSigning.Tests/Verification/VerificationTasks/CreationTimeVerificationTaskTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning.Verification.VerificationTasks {
+    public class CreationTimeVerificationTaskTests {
+        private readonly CreationTimeVerificationTask _sut;
+        private readonly ISystemClock _systemClock;
+
+        public CreationTimeVerificationTaskTests() {
+            FakeFactory.Create(out _systemClock);
+            _sut = new CreationTimeVerificationTask(_systemClock);
+        }
+
+        public class Verify : CreationTimeVerificationTaskTests {
+            private readonly HttpRequestForSigning _signedRequest;
+            private readonly Client _client;
+            private readonly Signature _signature;
+            private readonly Func<HttpRequestForSigning, Signature, Client, Task<Exception>> _method;
+            private readonly DateTimeOffset _now;
+
+            public Verify() {
+                _signature = (Signature) TestModels.Signature.Clone();
+                _signedRequest = (HttpRequestForSigning) TestModels.Request.Clone();
+                _client = (Client) TestModels.Client.Clone();
+                _method = (request, signature, client) => _sut.Verify(request, signature, client);
+
+                _now = _signature.Created.Value.AddSeconds(3);
+                A.CallTo(() => _systemClock.UtcNow).Returns(_now);
+            }
+
+            [Fact]
+            public async Task WhenSignatureDoesNotSpecifyACreationTime_ReturnsSignatureVerificationException() {
+                _signature.Created = null;
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().NotBeNull().And.BeAssignableTo<SignatureVerificationException>();
+            }
+
+            [Fact]
+            public async Task WhenSignatureCreationTimeIsInTheFuture_ReturnsSignatureVerificationException() {
+                _signature.Created = _now.AddSeconds(1);
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().NotBeNull().And.BeAssignableTo<SignatureVerificationException>();
+            }
+
+            [Fact]
+            public async Task WhenSignatureCreationTimeIsNow_ReturnsNull() {
+                _signature.Created = _now;
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().BeNull();
+            }
+
+            [Fact]
+            public async Task WhenSignatureCreationTimeIsInThePast_ReturnsNull() {
+                _signature.Created = _now.AddSeconds(-1);
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().BeNull();
+            }
+        }
+    }
+}
---
+++ src/HttpMessageSigning.Tests/Verification/VerificationTasks/ExpirationTimeVerificationTaskTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning.Verification.VerificationTasks {
+    public class ExpirationTimeVerificationTaskTests {
+        private readonly ExpirationTimeVerificationTask _sut;
+        private readonly ISystemClock _systemClock;
+
+        public ExpirationTimeVerificationTaskTests() {
+            FakeFactory.Create(out _systemClock);
+            _sut = new ExpirationTimeVerificationTask(_systemClock);
+        }
+
+        public class Verify : ExpirationTimeVerificationTaskTests {
+            private readonly HttpRequestForSigning _signedRequest;
+            private readonly Client _client;
+            private readonly Signature _signature;
+            private readonly Func<HttpRequestForSigning, Signature, Client, Task<Exception>> _method;
+            private readonly DateTimeOffset _now;
+
+            public Verify() {
+                _signature = (Signature) TestModels.Signature.Clone();
+                _signedRequest = (HttpRequestForSigning) TestModels.Request.Clone();
+                _client = (Client) TestModels.Client.Clone();
+                _method = (request, signature, client) => _sut.Verify(request, signature, client);
+
+                _now = _signature.Created.Value.AddSeconds(3);
+                A.CallTo(() => _systemClock.UtcNow).Returns(_now);
+            }
+
+            [Fact]
+            public async Task WhenSignatureDoesNotSpecifyAExpirationTime_ReturnsSignatureVerificationException() {
+                _signature.Expires = null;
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().NotBeNull().And.BeAssignableTo<SignatureVerificationException>();
+            }
+
+            [Fact]
+            public async Task WhenSignatureExpirationTimeIsInThePast_ReturnsSignatureVerificationException() {
+                _signature.Expires = _now.AddSeconds(-1);
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().NotBeNull().And.BeAssignableTo<SignatureVerificationException>();
+            }
+
+            [Fact]
+            public async Task WhenSignatureExpirationTimeIsNow_ReturnsNull() {
+                _signature.Expires = _now;
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().BeNull();
+            }
+
+            [Fact]
+            public async Task WhenSignatureExpirationTimeIsInTheFuture_ReturnsNull() {
+                _signature.Expires = _now.AddSeconds(1);
+
+                var actual = await _method(_signedRequest, _signature, _client);
+
+                actual.Should().BeNull();
+            }
+        }
+    }
+}
