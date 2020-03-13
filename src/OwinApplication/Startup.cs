6cda61ee0f04697200f2cc2fd476662f8c51ff84
David Lievrouw <david.lievrouw@recomatics.com>
2020-03-13 09:20:23 +0100

    Create empty OWIN sample app.

---
+++ src/OwinApplication/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinApplication.Startup))]
+
+namespace OwinApplication {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ src/OwinApplication/UserInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace OwinApplication {
+    public class UserInfoController : ApiController {
+        public IHttpActionResult Get() {
+            var authenticatedUser = User as ClaimsPrincipal;
+            if (authenticatedUser == null) return Unauthorized();
+            if (!authenticatedUser.Identity.IsAuthenticated) return Unauthorized();
+
+            return Ok(
+                authenticatedUser.Claims
+                    .Select(c => new {
+                        c.Type, c.Value
+                    })
+                    .ToList());
+        }
+    }
+}
