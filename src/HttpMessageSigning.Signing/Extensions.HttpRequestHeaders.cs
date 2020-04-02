6be2569433dd994d52edd577a35f6920d362d031
David Lievrouw <David.lievrouw@recomatics.com>
2020-04-02 10:44:33 +0200

    Add HttpRequestHeaders extension methods for signing.

---
+++ src/HttpMessageSigning.Signing/Extensions.HttpRequestHeaders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Dalion.HttpMessageSigning.Signing {
+    public static partial class Extensions {
+        public static void Set(this HttpRequestHeaders headers, string name, string value) {
+            if (headers == null) throw new ArgumentNullException(nameof(headers));
+            
+            if (headers.Contains(name)) headers.Remove(name);
+            headers.Add(name, value);
+        }        
+        
+        public static void Set(this HttpRequestHeaders headers, string name, IEnumerable<string> values) {
+            if (headers == null) throw new ArgumentNullException(nameof(headers));
+            
+            if (headers.Contains(name)) headers.Remove(name);
+            headers.Add(name, values);
+        }
+        
+        public static void Set(this HttpRequestHeaders headers, string name, params string[] values) {
+            if (headers == null) throw new ArgumentNullException(nameof(headers));
+            headers.Set(name, (IEnumerable<string>)values);
+        }
+    }
+}
---
+++ src/HttpMessageSigning.Tests/Signing/ExtensionsTests.HttpRequestHeaders.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning.Signing {
+    public class ExtensionsTests {
+        public class HttpRequestHeaders : ExtensionsTests {
+            private readonly System.Net.Http.Headers.HttpRequestHeaders _headers;
+
+            public HttpRequestHeaders() {
+                _headers = new HttpRequestMessage().Headers;
+                _headers.Add("existing_simple_header", "existing_value");
+                _headers.Add("existing_complex_header", new[] {"existing_value_01", "existing_value_02"});
+            }
+
+            public class AddSingle : HttpRequestHeaders {
+                [Fact]
+                public void GivenNullHeaders_ThrowsArgumentNullException() {
+                    Action act = () => Extensions.Set(null, "header01", "value01");
+
+                    act.Should().Throw<ArgumentNullException>();
+                }
+
+                [Fact]
+                public void WhenHeaderAlreadyExists_ReplacesValue() {
+                    _headers.Set("existing_simple_header", "value01");
+
+                    _headers.GetValues("existing_simple_header").Should().BeEquivalentTo("value01");
+                }
+
+                [Fact]
+                public void WhenHeaderDoesNotExist_AddsValue() {
+                    _headers.Set("i_dont_exist", "value01");
+
+                    _headers.GetValues("i_dont_exist").Should().BeEquivalentTo("value01");
+                }
+
+                [Theory]
+                [InlineData(null)]
+                [InlineData("")]
+                public void GivenNullOrEmptyName_ThrowsArgumentException(string nullOrEmpty) {
+                    Action act = () => _headers.Set(nullOrEmpty, "value01");
+
+                    act.Should().Throw<ArgumentException>();
+                }
+
+                [Theory]
+                [InlineData(null)]
+                [InlineData("")]
+                public void GivenNullOrEmptyValue_SetsValueToEmpty(string nullOrEmpty) {
+                    _headers.Set("i_dont_exist", nullOrEmpty);
+
+                    _headers.GetValues("i_dont_exist").Should().BeEquivalentTo("");
+                }
+            }
+
+            public class AddMultiple : HttpRequestHeaders {
+                [Fact]
+                public void GivenNullHeaders_ThrowsArgumentNullException() {
+                    Action act = () => Extensions.Set(null, "header01", new[] {"value01"});
+
+                    act.Should().Throw<ArgumentNullException>();
+                }
+
+                [Fact]
+                public void WhenHeaderAlreadyExists_ReplacesValues() {
+                    _headers.Set("existing_complex_header", "value01", "value02");
+
+                    _headers.GetValues("existing_complex_header").Should().BeEquivalentTo("value01", "value02");
+                }
+
+                [Fact]
+                public void WhenHeaderAlreadyExists_AndItsCurrentlyASingleValue_ReplacesValues() {
+                    _headers.Set("existing_simple_header", "value01");
+
+                    _headers.Set("existing_simple_header", "value01", "value02");
+
+                    _headers.GetValues("existing_simple_header").Should().BeEquivalentTo("value01", "value02");
+                }
+
+                [Fact]
+                public void WhenHeaderDoesNotExist_AddsValues() {
+                    _headers.Set("i_dont_exist", new List<string>(new[] {"value01", "value02"}));
+
+                    _headers.GetValues("i_dont_exist").Should().BeEquivalentTo("value01", "value02");
+                }
+
+                [Theory]
+                [InlineData(null)]
+                [InlineData("")]
+                public void GivenNullOrEmptyName_ThrowsArgumentException(string nullOrEmpty) {
+                    Action act = () => _headers.Set(nullOrEmpty, "value01", "value02");
+
+                    act.Should().Throw<ArgumentException>();
+                }
+
+                [Theory]
+                [InlineData(null)]
+                [InlineData("")]
+                public void GivenNullOrEmptyValues_SetsValueToEmpty(string nullOrEmpty) {
+                    _headers.Set("existing_complex_header", new[] {nullOrEmpty});
+
+                    _headers.GetValues("existing_complex_header").Should().BeEquivalentTo("");
+                }
+
+                [Fact]
+                public void GivenNoValues_RemovesHeader() {
+                    _headers.Set("existing_complex_header", Enumerable.Empty<string>());
+
+                    _headers.Contains("existing_complex_header").Should().BeFalse();
+                }
+            }
+        }
+    }
+}
