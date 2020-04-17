7fb12f175007acc2bd5d49edaefbc1a1db4c977f
David Lievrouw <david.lievrouw@recomatics.com>
2020-04-17 11:27:27 +0200

    Add a benchmark project, with a simple signing benchmark.

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark {
+    internal class Program {
+        private static void Main(string[] args) {
+            var summary = BenchmarkRunner.Run<SignRequestWithoutDigest>();
+        }
+    }
+}
