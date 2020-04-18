c395ac3d67b69fc4f1e2a972d4a70373c71b2ecf
David Lievrouw <david.lievrouw@recomatics.com>
2020-04-18 19:51:21 +0200

    Add system test for extension point.

---
+++ src/HttpMessageSigning.Tests/SystemTests/ExtensionPoints/UpdateSigningSettingsBeforeSigning.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Dalion.HttpMessageSigning.Signing;
+using Dalion.HttpMessageSigning.Verification;
+using Dalion.HttpMessageSigning.Verification.AspNetCore;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dalion.HttpMessageSigning.SystemTests.ExtensionPoints {
+    public class UpdateSigningSettingsBeforeSigning : IDisposable {
+        private readonly ITestOutputHelper _output;
+        private readonly ServiceProvider _serviceProvider;
+        private readonly IRequestSignerFactory _requestSignerFactory;
+        private readonly IRequestSignatureVerifier _verifier;
+        private readonly SignedRequestAuthenticationOptions _options;
+
+        public UpdateSigningSettingsBeforeSigning(ITestOutputHelper output) {
+            _output = output;
+            _serviceProvider = new ServiceCollection().Configure(ConfigureServices).BuildServiceProvider();
+            _requestSignerFactory = _serviceProvider.GetRequiredService<IRequestSignerFactory>();
+            _verifier = _serviceProvider.GetRequiredService<IRequestSignatureVerifier>();
+            _options = new SignedRequestAuthenticationOptions();
+        }
+
+        public void Dispose() {
+            _serviceProvider?.Dispose();
+            _requestSignerFactory?.Dispose();
+            _verifier?.Dispose();
+        }
+        
+        [Fact]
+        public async Task CanUpdateSigningSettingsBeforeSigning() {
+            var request = new HttpRequestMessage {
+                RequestUri = new Uri("https://httpbin.org/post"),
+                Method = HttpMethod.Get,
+                Headers = {
+                    {"Dalion-App-Id", "ringor"}
+                }
+            };
+            
+            var requestSigner = _requestSignerFactory.CreateFor("e0e8dcd638334c409e1b88daf821d135");
+            await requestSigner.Sign(request);
+
+            request.Headers.Authorization.Parameter.Should().NotContain("nonce");
+        }
+
+        private void ConfigureServices(IServiceCollection services) {
+            services
+                .AddHttpMessageSigning(
+                    new KeyId("e0e8dcd638334c409e1b88daf821d135"),
+                    provider => new SigningSettings {
+                        SignatureAlgorithm = SignatureAlgorithm.CreateForSigning("yumACY64r%hm"),
+                        DigestHashAlgorithm = HashAlgorithmName.SHA256,
+                        Expires = TimeSpan.FromMinutes(1),
+                        EnableNonce = true,
+                        Headers = new[] {
+                            (HeaderName) "Dalion-App-Id"
+                        },
+                        Events = new RequestSigningEvents {
+                            OnRequestSigning = OnRequestSigning
+                        }
+                    })
+                .AddHttpMessageSignatureVerification(provider => {
+                    var clientStore = new InMemoryClientStore();
+                    clientStore.Register(new Client(
+                        new KeyId("e0e8dcd638334c409e1b88daf821d135"),
+                        "HttpMessageSigningSampleHMAC",
+                        SignatureAlgorithm.CreateForVerification("yumACY64r%hm"),
+                        TimeSpan.FromMinutes(5),
+                        new Claim(SignedHttpRequestClaimTypes.Role, "users.read")));
+                    return clientStore;
+                });
+        }
+
+        private static Task OnRequestSigning(HttpRequestMessage request, SigningSettings settings) {
+            settings.EnableNonce = false;
+            return Task.CompletedTask;
+        }
+    }
+}
