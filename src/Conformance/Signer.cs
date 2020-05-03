7780322041af329d53aec6eed27437d25780c409
David Lievrouw <David.lievrouw@recomatics.com>
2020-05-03 03:11:04 +0200

    Conformance: Take key-type argument into account.

--- src/Conformance/Signer.cs
+++ src/Conformance/Signer.cs
@@ -21,53 +21,76 @@ namespace Conformance {
         }
 
         public async Task<int> Run(SignOptions options, string httpMessage) {
-            RSAParameters privateKey;
-            using (var stream = File.OpenRead(options.PrivateKey)) {
-                using (var reader = new PemReader(stream))
-                {
-                    privateKey = reader.ReadRsaKey();
-                }
-            }
-
             if (options.Algorithm != "hs2019") {
                 throw new HttpMessageSigningException("Unsupported algorithm.");
             }
+            
+            ISignatureAlgorithm signatureAlgorithm;
+            if (string.IsNullOrEmpty(options.KeyType) || options.KeyType.Equals("RSA", StringComparison.OrdinalIgnoreCase)) {
+                RSAParameters rsaPrivateKey;
+                using (var stream = File.OpenRead(options.PrivateKey)) {
+                    using (var reader = new PemReader(stream)) {
+                        rsaPrivateKey = reader.ReadRsaKey();
+                    }
+                }
+                signatureAlgorithm = RSASignatureAlgorithm.CreateForSigning(HashAlgorithmName.SHA256, rsaPrivateKey);
+            } else if (options.KeyType.Equals("P256", StringComparison.OrdinalIgnoreCase) || options.KeyType.Equals("ECDSA", StringComparison.OrdinalIgnoreCase)) {
+                ECParameters ecPrivateKey;
+                using (var stream = File.OpenRead(options.PrivateKey)) {
+                    using (var reader = new StreamReader(stream)) {
+                        var fileContents = reader.ReadToEnd();
+                        var lines = fileContents.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+                        lines = lines.Skip(1).Take(lines.Length - 2).ToArray();
+                        var pem = string.Join("", lines);
+                        var ecdsa = ECDsa.Create();
+                        var derArray = Convert.FromBase64String(pem);
+                        ecdsa.ImportPkcs8PrivateKey(derArray, out _);
+                        ecPrivateKey = ecdsa.ExportParameters(true);
+                    }
+                }
+                signatureAlgorithm = ECDsaSignatureAlgorithm.CreateForSigning(HashAlgorithmName.SHA256, ecPrivateKey);
+            } else if (options.KeyType.Equals("HMAC", StringComparison.OrdinalIgnoreCase)) {
+                signatureAlgorithm = new HMACSignatureAlgorithm(options.PrivateKey, HashAlgorithmName.SHA256);
+            }
+            else {
+                throw new NotSupportedException("The specified key type is not supported.");
+            }
 
             var signingSettings = new SigningSettings {
-                SignatureAlgorithm = RSASignatureAlgorithm.CreateForSigning(HashAlgorithmName.SHA256, privateKey),
+                SignatureAlgorithm = signatureAlgorithm,
                 EnableNonce = false,
                 DigestHashAlgorithm = default,
                 AutomaticallyAddRecommendedHeaders = false,
                 Headers = options.Headers
-                    ?.Split(new [] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                    ?.Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
                     .Select(h => new HeaderName(h))
                     .ToArray()
             };
             var signer = _requestSignerFactory.Create(
                 new KeyId("test"),
                 signingSettings);
-            
+
             var request = HttpRequestMessageParser.Parse(httpMessage);
-            
+
             var created = DateTimeOffset.UtcNow;
             if (!string.IsNullOrEmpty(options.Created)) {
                 var createdUnix = int.Parse(options.Created);
                 created = DateTimeOffset.FromUnixTimeSeconds(createdUnix);
             }
-            
+
             var expires = signingSettings.Expires;
             if (!string.IsNullOrEmpty(options.Expires)) {
                 var expiresUnix = int.Parse(options.Expires);
                 var expiresAbsolute = DateTimeOffset.FromUnixTimeSeconds(expiresUnix);
                 expires = expiresAbsolute - created;
             }
-            
+
             await signer.Sign(request, created, expires);
-            
+
             Log.Information(request.Headers.Authorization.Scheme + " " + request.Headers.Authorization.Parameter);
-            
+
             Console.Out.Flush();
-            
+
             return 0;
         }
     }
