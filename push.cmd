a2f441c16490bc1f7c9cd9394e8090e86e813220
David Lievrouw <David.lievrouw@recomatics.com>
2020-05-08 14:25:59 +0200

    Update nuget push helper script.

--- push.cmd
+++ push.cmd
@@ -3,16 +3,31 @@ cls
 SET DIR=%~dp0%
 SET KEY=s3cr37
 SET SOURCE=https://api.nuget.org/v3/index.json
-SET VERSION=4.0.0
 
-ECHO Press [ENTER] to push the packages to nuget.org
+for /f "delims=" %%x in (version.txt) do set VERSION=%%x
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning v%VERSION% to nuget.org
 PAUSE
-
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.%VERSION%.nupkg -k %KEY% -s %SOURCE%
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning.Signing v%VERSION% to nuget.org
+PAUSE
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.Signing.%VERSION%.nupkg -k %KEY% -s %SOURCE%
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning.Verification v%VERSION% to nuget.org
+PAUSE
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.Verification.%VERSION%.nupkg -k %KEY% -s %SOURCE%
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning.Verification.AspNetCore v%VERSION% to nuget.org
+PAUSE
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.Verification.AspNetCore.%VERSION%.nupkg -k %KEY% -s %SOURCE%
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning.Verification.MongoDb v%VERSION% to nuget.org
+PAUSE
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.Verification.MongoDb.%VERSION%.nupkg -k %KEY% -s %SOURCE%
+
+ECHO Press [ENTER] to push the package Dalion.HttpMessageSigning.Verification.Owin v%VERSION% to nuget.org
+PAUSE
 dotnet nuget push %DIR%dist\NuGetPackages\Dalion.HttpMessageSigning.Verification.Owin.%VERSION%.nupkg -k %KEY% -s %SOURCE%
 
 PAUSE
