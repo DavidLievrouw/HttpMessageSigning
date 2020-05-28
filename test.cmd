4b8a16d94523555487462601ffe85c37f41de8b3
David Lievrouw <David.lievrouw@recomatics.com>
2020-05-28 16:26:39 +0200

    #6 Update publish and test scripts.

--- test.cmd
+++ test.cmd
@@ -5,7 +5,10 @@ SET SRCDIR=%DIR%\src
 SET PRODUCT=HttpMessageSigning
 
 dotnet test %SRCDIR%\%PRODUCT%.Tests\%PRODUCT%.Tests.csproj
+dotnet test %SRCDIR%\%PRODUCT%.Verification.AspNetCore.Tests\%PRODUCT%.Verification.AspNetCore.Tests.csproj
 dotnet test %SRCDIR%\%PRODUCT%.Verification.Owin.Tests\%PRODUCT%.Verification.Owin.Tests.csproj
+dotnet test %SRCDIR%\%PRODUCT%.Verification.MongoDb.Tests\%PRODUCT%.Verification.MongoDb.Tests.csproj
+dotnet test %SRCDIR%\%PRODUCT%.SystemTests\%PRODUCT%.SystemTests.csproj
 dotnet test %SRCDIR%\Conformance.Tests\Conformance.Tests.csproj
 
 pause
