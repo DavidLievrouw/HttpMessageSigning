026551328c721314159fca80010fddd9118dce84
David Lievrouw <David.lievrouw@recomatics.com>
2020-08-18 15:44:53 +0200

    #8 Do not throw an InvalidSignatureException upon invalid signature. It's not an Exception, per se, it's a very common case. So just use failure results, instead.

--- src/HttpMessageSigning.Verification/RequestSignatureVerificationOrchestrator.cs
+++ src/HttpMessageSigning.Verification/RequestSignatureVerificationOrchestrator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -23,47 +24,41 @@ namespace Dalion.HttpMessageSigning.Verification {
         public async Task<RequestSignatureVerificationResult> VerifySignature(HttpRequestForVerification request) {
             if (request == null) throw new ArgumentNullException(nameof(request));
 
-            Client client = null;
-            try {
-                if (request.Signature == null) {
-                    throw new InvalidSignatureException("The signature is missing.");
-                }
-                
-                try {
-                    request.Signature.Validate();
-                }
-                catch (ValidationException ex) {
-                    throw new InvalidSignatureException("The signature is invalid. See inner exception.", ex);
-                }
-                
-                client = await _clientStore.Get(request.Signature.KeyId).ConfigureAwait(continueOnCapturedContext: false);
-                if (client == null) {
-                    var failure = SignatureVerificationFailure.InvalidClient($"No {nameof(Client)}s with id '{request.Signature.KeyId}' are registered in the server store.");
-                    _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Code, failure.Message);
-                    return new RequestSignatureVerificationResultFailure(client, request.Signature, failure);
-                }
-                
-                var verificationFailure = await _signatureVerifier.VerifySignature(request, client).ConfigureAwait(continueOnCapturedContext: false);
-
-                var verificationResultCreator = _verificationResultCreatorFactory.Create(client, request.Signature);
-                var result = verificationFailure == null
-                    ? verificationResultCreator.CreateForSuccess()
-                    : verificationResultCreator.CreateForFailure(verificationFailure);
-
-                if (result is RequestSignatureVerificationResultSuccess success) {
-                    _logger?.LogDebug($"Request signature verification succeeded for principal {success.Principal?.Identity?.Name ?? "[null]"}.");
-                }
-                else if (result is RequestSignatureVerificationResultFailure failure) {
-                    _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Failure.Code, failure.Failure.Message);
-                }
-
-                return result;
-            }
-            catch (InvalidSignatureException ex) {
-                var failure = SignatureVerificationFailure.InvalidSignature(ex.Message, ex);
+            if (request.Signature == null) {
+                var failure = SignatureVerificationFailure.InvalidSignature("The signature is missing from the request.");
                 _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Code, failure.Message);
-                return new RequestSignatureVerificationResultFailure(client, request.Signature, failure);
+                return new RequestSignatureVerificationResultFailure(client: null, request.Signature, failure);
             }
+
+            var firstValidationError = request.Signature.GetValidationErrors().FirstOrDefault();
+            if (firstValidationError != default) {
+                var failure = SignatureVerificationFailure.InvalidSignature($"The signature is invalid: {firstValidationError.Message}");
+                _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Code, failure.Message);
+                return new RequestSignatureVerificationResultFailure(client: null, request.Signature, failure);
+            }
+
+            var client = await _clientStore.Get(request.Signature.KeyId).ConfigureAwait(continueOnCapturedContext: false);
+            if (client == null) {
+                var failure = SignatureVerificationFailure.InvalidClient($"No {nameof(Client)}s with id '{request.Signature.KeyId}' are registered in the server store.");
+                _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Code, failure.Message);
+                return new RequestSignatureVerificationResultFailure(client: null, request.Signature, failure);
+            }
+
+            var verificationFailure = await _signatureVerifier.VerifySignature(request, client).ConfigureAwait(continueOnCapturedContext: false);
+
+            var verificationResultCreator = _verificationResultCreatorFactory.Create(client, request.Signature);
+            var result = verificationFailure == null
+                ? verificationResultCreator.CreateForSuccess()
+                : verificationResultCreator.CreateForFailure(verificationFailure);
+
+            if (result is RequestSignatureVerificationResultSuccess success) {
+                _logger?.LogDebug($"Request signature verification succeeded for principal {success.Principal?.Identity?.Name ?? "[null]"}.");
+            }
+            else if (result is RequestSignatureVerificationResultFailure failure) {
+                _logger?.LogWarning("Request signature verification failed ({0}): {1}", failure.Failure.Code, failure.Failure.Message);
+            }
+
+            return result;
         }
 
         public void Dispose() {
