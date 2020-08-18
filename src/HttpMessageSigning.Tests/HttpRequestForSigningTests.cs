3fcc723806c4166a631098e1a5cf9a922e665226
David Lievrouw <David.lievrouw@recomatics.com>
2020-08-18 14:45:27 +0200

    #8 Add missing tests for new pocos.

---
+++ src/HttpMessageSigning.Tests/HttpRequestForSigningTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning {
+    public class HttpRequestForSigningTests {
+        private readonly HttpRequestForSigning _sut;
+
+        public HttpRequestForSigningTests() {
+            _sut = new HttpRequestForSigning {
+                Headers = new HeaderDictionary {
+                    {"h1", new StringValues(new[] {"v1", "v2"})},
+                    {"h2", new StringValues(new[] {"v3"})},
+                    {"h3", StringValues.Empty}
+                },
+                Method = HttpMethod.Options,
+                RequestUri = "https://unittest.com:9000/api?test=true"
+            };
+        }
+
+        public class Clone : HttpRequestForSigningTests {
+            [Fact]
+            public void ReturnsNewInstanceWithExpectedValues() {
+                var actual = _sut.Clone();
+                actual.Should().NotBe(_sut);
+                actual.Should().BeEquivalentTo(_sut);
+            }
+        }
+    }
+}
---
+++ src/HttpMessageSigning.Tests/Verification/HttpRequestForVerificationTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning.Verification {
+    public class HttpRequestForVerificationTests {
+        private readonly HttpRequestForVerification _sut;
+
+        public HttpRequestForVerificationTests() {
+            _sut = new HttpRequestForVerification {
+                Headers = new HeaderDictionary {
+                    {"h1", new StringValues(new[] {"v1", "v2"})},
+                    {"h2", new StringValues(new[] {"v3"})},
+                    {"h3", StringValues.Empty}
+                },
+                Method = HttpMethod.Options,
+                RequestUri = "https://unittest.com:9000/api?test=true",
+                Signature = new Signature {
+                    KeyId = new KeyId("abc123"),
+                    Algorithm = "hs2019",
+                    String = "xyz001"
+                }
+            };
+        }
+
+        public class Clone : HttpRequestForVerificationTests {
+            [Fact]
+            public void ReturnsNewInstanceWithExpectedValues() {
+                var actual = _sut.Clone();
+                actual.Should().NotBe(_sut);
+                actual.Should().BeEquivalentTo(_sut);
+            }
+        }
+    }
+}
