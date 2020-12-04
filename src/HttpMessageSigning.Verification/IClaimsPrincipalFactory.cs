e935dfbc79ee0c34fdee5c968901823fbac49333
David Lievrouw <David.lievrouw@recomatics.com>
2020-12-04 09:59:42 +0100

    #23 Add XML comments to get rid of compile-time warnings.

--- src/HttpMessageSigning.Verification/IClaimsPrincipalFactory.cs
+++ src/HttpMessageSigning.Verification/IClaimsPrincipalFactory.cs
@@ -1,7 +1,15 @@
 using System.Security.Claims;
 
 namespace Dalion.HttpMessageSigning.Verification {
+    /// <summary>
+    ///     Represents an object that creates authenticated principal, after successful signature verification.
+    /// </summary>
     public interface IClaimsPrincipalFactory {
+        /// <summary>
+        ///     Create an authenticated principal, using the settings of the specified <see cref="Client" />.
+        /// </summary>
+        /// <param name="client">The <see cref="Client" /> whose settings were used to verify the signature.</param>
+        /// <returns>An authenticated <see cref="ClaimsPrincipal" />.</returns>
         ClaimsPrincipal CreateForClient(Client client);
     }
 }
