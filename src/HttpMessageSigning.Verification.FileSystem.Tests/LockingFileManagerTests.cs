d379179bec9e82f8a7a91b8a7e6c3cffd6df2cc7
David Lievrouw <David.lievrouw@recomatics.com>
2021-01-26 16:59:01 +0100

    #32 Add LockingClientStore decorator.

---
+++ src/HttpMessageSigning.Verification.FileSystem.Tests/LockingFileManagerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using Dalion.HttpMessageSigning.TestUtils;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Dalion.HttpMessageSigning.Verification.FileSystem {
+    public class LockingClientStoreTests : IDisposable {
+        private readonly IClientStore _decorated;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly ISemaphoreFactory _semaphoreFactory;
+        private readonly LockingClientStore _sut;
+
+        public LockingClientStoreTests() {
+            FakeFactory.Create(out _decorated, out _semaphoreFactory);
+            _semaphore = new SemaphoreSlim(1, 1);
+            A.CallTo(() => _semaphoreFactory.CreateLock())
+                .Returns(_semaphore);
+            _sut = new LockingClientStore(_decorated, _semaphoreFactory);
+        }
+
+        public void Dispose() {
+            _semaphore?.Dispose();
+            _sut?.Dispose();
+        }
+
+        public class Register : LockingClientStoreTests {
+            [Fact]
+            public async Task CallsDecoratedService() {
+                var hmac = new HMACSignatureAlgorithm("s3cr3t", HashAlgorithmName.SHA384);
+                var client = new Client(
+                    "c1",
+                    "app one",
+                    hmac,
+                    TimeSpan.FromMinutes(1),
+                    TimeSpan.FromMinutes(2),
+                    RequestTargetEscaping.RFC2396,
+                    new Claim("company", "Dalion"),
+                    new Claim("scope", "HttpMessageSigning"));
+
+                await _sut.Register(client);
+
+                A.CallTo(() => _decorated.Register(client))
+                    .MustHaveHappened();
+            }
+        }
+
+        public class Get : LockingClientStoreTests {
+            [Fact]
+            public async Task ReturnsResultFromDecoratedService() {
+                var hmac = new HMACSignatureAlgorithm("s3cr3t", HashAlgorithmName.SHA384);
+                var client = new Client(
+                    "c1",
+                    "app one",
+                    hmac,
+                    TimeSpan.FromMinutes(1),
+                    TimeSpan.FromMinutes(2),
+                    RequestTargetEscaping.RFC2396,
+                    new Claim("company", "Dalion"),
+                    new Claim("scope", "HttpMessageSigning"));
+
+                A.CallTo(() => _decorated.Get(client.Id))
+                    .Returns(client);
+
+                var actual = await _sut.Get(client.Id);
+
+                actual.Should().Be(client);
+            }
+        }
+    }
+}
