47bed187354412cd657fc6f50ef2c394a401bfb3
David Lievrouw <David.lievrouw@recomatics.com>
2021-01-28 10:50:02 +0100

    Minor tweak to the coverage script

--- coverage.cmd
+++ coverage.cmd
@@ -6,25 +6,25 @@ SET SRCDIR=%DIR%\src
 IF EXIST %SRCDIR%\coverage.xml DEL /F %SRCDIR%\coverage.xml
 IF EXIST "%SRCDIR%\coverage-*.json" DEL /F %SRCDIR%\coverage-*.json
 
-dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework net472 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning*]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /maxcpucount:1
+dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework net472 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /maxcpucount:1
 if errorlevel 1 (
    echo One or more tests failed.
    exit /b %errorlevel%
 )
 
-dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework netcoreapp2.1 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning*]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.net472.json" /maxcpucount:1
+dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework netcoreapp2.1 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.net472.json" /maxcpucount:1
 if errorlevel 1 (
    echo One or more tests failed.
    exit /b %errorlevel%
 )
 
-dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework netcoreapp3.1 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning*]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.netcoreapp2.1.json" /maxcpucount:1
+dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework netcoreapp3.1 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.netcoreapp2.1.json" /maxcpucount:1
 if errorlevel 1 (
    echo One or more tests failed.
    exit /b %errorlevel%
 )
 
-dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework net5 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning*]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.netcoreapp3.1.json" /maxcpucount:1
+dotnet test %SRCDIR%\HttpMessageSigning.Tests\HttpMessageSigning.Tests.csproj --framework net5 /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:IncludeTestAssembly=true /p:UseSourceLink=true /p:Include="[Dalion.HttpMessageSigning]*" /p:Exclude=\"[*Test*]*,[Dalion.HttpMessageSigning.TestUtils]*\" /p:ExcludeByAttribute=\"Obsolete,ObsoleteAttribute,GeneratedCodeAttribute,CompilerGeneratedAttribute,ExcludeFromCodeCoverage,ExcludeFromCodeCoverageAttribute\" /p:CoverletOutput="../coverage-core.json" /p:MergeWith="../coverage-core.netcoreapp3.1.json" /maxcpucount:1
 if errorlevel 1 (
    echo One or more tests failed.
    exit /b %errorlevel%
