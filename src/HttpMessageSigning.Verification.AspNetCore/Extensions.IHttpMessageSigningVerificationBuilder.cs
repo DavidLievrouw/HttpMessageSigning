c4a8317cf037893063baaccb9a328a8a960f9bcc
David Lievrouw <david.lievrouw@recomatics.com>
2021-03-06 17:20:53 +0100

    #33 Add UseSignatureParser convenience methods.

--- src/HttpMessageSigning.Verification.AspNetCore/Extensions.IHttpMessageSigningVerificationBuilder.cs
+++ src/HttpMessageSigning.Verification.AspNetCore/Extensions.IHttpMessageSigningVerificationBuilder.cs
@@ -18,5 +18,41 @@ namespace Dalion.HttpMessageSigning.Verification.AspNetCore {
 
             return builder;
         }
+
+        /// <summary>Configures HTTP message signature verification to use the specified <see cref="ISignatureParser" />.</summary>
+        /// <param name="builder">The <see cref="IHttpMessageSigningVerificationBuilder" /> that is used to configure verification.</param>
+        /// <typeparam name="TSignatureParser">The type of the <see cref="ISignatureParser" /> that is to be used.</typeparam>
+        /// <returns>The <see cref="IHttpMessageSigningVerificationBuilder" /> that can be used to continue configuring the verification settings.</returns>
+        [ExcludeFromCodeCoverage]
+        public static IHttpMessageSigningVerificationBuilder UseSignatureParser<TSignatureParser>(this IHttpMessageSigningVerificationBuilder builder)
+            where TSignatureParser : ISignatureParser {
+            return builder.UseSignatureParser(provider => provider.GetRequiredService<TSignatureParser>());
+        }
+
+        /// <summary>Configures HTTP message signature verification to use the specified <see cref="ISignatureParser" />.</summary>
+        /// <param name="builder">The <see cref="IHttpMessageSigningVerificationBuilder" /> that is used to configure verification.</param>
+        /// <param name="signatureParser">The <see cref="ISignatureParser" /> that is to be used.</param>
+        /// <returns>The <see cref="IHttpMessageSigningVerificationBuilder" /> that can be used to continue configuring the verification settings.</returns>
+        [ExcludeFromCodeCoverage]
+        public static IHttpMessageSigningVerificationBuilder UseSignatureParser(this IHttpMessageSigningVerificationBuilder builder, ISignatureParser signatureParser) {
+            if (signatureParser == null) throw new ArgumentNullException(nameof(signatureParser));
+
+            return builder.UseSignatureParser(provider => signatureParser);
+        }
+
+        /// <summary>Configures HTTP message signature verification to use the specified <see cref="ISignatureParser" />.</summary>
+        /// <param name="builder">The <see cref="IHttpMessageSigningVerificationBuilder" /> that is used to configure verification.</param>
+        /// <param name="signatureParserFactory">The factory that creates the <see cref="ISignatureParser" /> that is to be used.</param>
+        /// <returns>The <see cref="IHttpMessageSigningVerificationBuilder" /> that can be used to continue configuring the verification settings.</returns>
+        [ExcludeFromCodeCoverage]
+        public static IHttpMessageSigningVerificationBuilder UseSignatureParser(
+            this IHttpMessageSigningVerificationBuilder builder,
+            Func<IServiceProvider, ISignatureParser> signatureParserFactory) {
+            if (signatureParserFactory == null) throw new ArgumentNullException(nameof(signatureParserFactory));
+
+            builder.Services.AddSingleton(signatureParserFactory);
+
+            return builder;
+        }
     }
 }
