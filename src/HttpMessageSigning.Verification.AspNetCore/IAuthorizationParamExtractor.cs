34c90d48d512ad8a73666b224dd8d830851b8966
David Lievrouw <david.lievrouw@recomatics.com>
2021-03-07 08:46:00 +0100

    #33 Make it possible to register a custom implementation of IAuthorizationHeaderExtractor.

--- src/HttpMessageSigning.Verification.AspNetCore/IAuthorizationParamExtractor.cs
+++ src/HttpMessageSigning.Verification.AspNetCore/IAuthorizationParamExtractor.cs
@@ -2,7 +2,16 @@ using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Http;
 
 namespace Dalion.HttpMessageSigning.Verification.AspNetCore {
-    internal interface IAuthorizationHeaderExtractor {
+    /// <summary>
+    ///     Extracts the values of the header that contains the signature.
+    /// </summary>
+    public interface IAuthorizationHeaderExtractor {
+        /// <summary>
+        ///     Extract the values of the header that contains the signature.
+        /// </summary>
+        /// <param name="request">The <see cref="HttpRequest" /> to extract the values from.</param>
+        /// <returns>A <see cref="AuthenticationHeaderValue" /> that contains the Scheme and Parameter, that the specified <see cref="HttpRequest" /> contains.</returns>
+        /// <remarks>This method returns <see langword="null" /> when the request does not contain authentication information.</remarks>
         AuthenticationHeaderValue Extract(HttpRequest request);
     }
 }
