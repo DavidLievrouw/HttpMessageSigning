7cb1edcc1ea2964a1d0a4513c2902fd70747a47c
David Lievrouw <david.lievrouw@recomatics.com>
2022-08-31 15:28:42 +0200

    #38 Fix failing tests.

--- src/HttpMessageSigning.Tests/HashAlgorithmFactoryTests.cs
+++ src/HttpMessageSigning.Tests/HashAlgorithmFactoryTests.cs
@@ -6,15 +6,16 @@ using Xunit;
 namespace Dalion.HttpMessageSigning {
     public class HashAlgorithmFactoryTests {
         [Theory]
-        [InlineData("MD5", "MD5CryptoServiceProvider")]
-        [InlineData("SHA1", "SHA1CryptoServiceProvider")]
-        [InlineData("SHA256", "SHA256Managed")]
-        [InlineData("SHA384", "SHA384Managed")]
-        [InlineData("SHA512", "SHA512Managed")]
+        [InlineData("MD5", "MD5")]
+        [InlineData("SHA1", "SHA1")]
+        [InlineData("SHA256", "SHA256")]
+        [InlineData("SHA384", "SHA384")]
+        [InlineData("SHA512", "SHA512")]
         public void WhenAlgorithmIsSupported_ReturnsExpectedAlgorithm(string algorithm, string expectedType) {
             var actual = HashAlgorithmFactory.Create(new HashAlgorithmName(algorithm));
             actual.Should().NotBeNull();
-            actual.GetType().Name.Should().Be(expectedType);
+            actual.Should().BeAssignableTo<HashAlgorithm>();
+            actual.GetType().FullName.Should().Contain(expectedType + "+");
         }
 
         [Fact]
