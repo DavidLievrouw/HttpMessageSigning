195f3dfdb83d88717c00eefe9aecdc894e87e449
David Lievrouw <david.lievrouw@recomatics.com>
2024-09-30 21:03:42 +0200

    #46 Finetuning

--- src/HttpMessageSigning.Signing/Extensions.HttpRequestMessage.cs
+++ src/HttpMessageSigning.Signing/Extensions.HttpRequestMessage.cs
@@ -6,22 +6,28 @@ namespace Dalion.HttpMessageSigning.Signing {
     public static partial class Extensions {
         internal static HttpRequestForSigning ToHttpRequestForSigning(this HttpRequestMessage httpRequestMessage) {
             if (httpRequestMessage == null) return null;
-            
+
             var requestForSigning = new HttpRequestForSigning {
                 Method = httpRequestMessage.Method,
                 RequestUri = httpRequestMessage.RequestUri
             };
 
             foreach (var header in httpRequestMessage.Headers) {
-                requestForSigning.Headers[header.Key] = new StringValues(header.Value.ToArray());
+                var values = header.Value.ToArray();
+                requestForSigning.Headers[header.Key] = values.Length == 1
+                    ? new StringValues(values[0])
+                    : new StringValues(values);
             }
-            
+
             if (httpRequestMessage.Content?.Headers?.Any() ?? false) {
                 foreach (var contentHeader in httpRequestMessage.Content.Headers) {
-                    requestForSigning.Headers[contentHeader.Key] = new StringValues(contentHeader.Value.ToArray());
+                    var values = contentHeader.Value.ToArray();
+                    requestForSigning.Headers[contentHeader.Key] = values.Length == 1
+                        ? new StringValues(values[0])
+                        : new StringValues(values);
                 }
             }
-            
+
             return requestForSigning;
         }
     }
--- src/HttpMessageSigning/SigningString/DefaultHeaderAppender.cs
+++ src/HttpMessageSigning/SigningString/DefaultHeaderAppender.cs
@@ -1,24 +1,63 @@
 using System;
-using System.Linq;
 using System.Text;
 
 namespace Dalion.HttpMessageSigning.SigningString {
     internal class DefaultHeaderAppender : IHeaderAppender {
         private readonly HttpRequestForSignatureString _request;
-        
+
         public DefaultHeaderAppender(HttpRequestForSignatureString request) {
             _request = request ?? throw new ArgumentNullException(nameof(request));
         }
-        
+
         public void Append(HeaderName header, StringBuilder sb) {
             var isPresent = _request.Headers.TryGetValues(header, out var headerValues);
-            
+
             if (!isPresent) throw new HttpMessageSigningException($"Header '{header}' was required to create the signature, but does not exist on the request message to sign.");
-            
-            var headerToAppend = new Header(header, headerValues.Select(SanitizeHeaderValue).ToArray());
+
+            var values = new string[headerValues.Count];
+            for (var i = 0; i < headerValues.Count; i++) {
+                values[i] = SanitizeHeaderValue(headerValues[i]);
+            }
+
+            var headerToAppend = new Header(header, values);
             headerToAppend.Append(sb);
         }
 
+#if NET6_0_OR_GREATER
+        private static string SanitizeHeaderValue(string input) {
+            if (input == null) return null;
+            if (input.IndexOf('\n') < 0) return input.Trim();
+
+            var sb = new StringBuilder(input.Length);
+            var span = input.AsSpan();
+            var inWhitespace = true;
+
+            foreach (var c in span) {
+                if (c == '\n') {
+                    if (!inWhitespace) {
+                        sb.Append(' ');
+                        inWhitespace = true;
+                    }
+                }
+                else if (char.IsWhiteSpace(c)) {
+                    if (!inWhitespace) {
+                        sb.Append(' ');
+                        inWhitespace = true;
+                    }
+                }
+                else {
+                    sb.Append(c);
+                    inWhitespace = false;
+                }
+            }
+
+            if (sb.Length > 0 && sb[^1] == ' ') {
+                sb.Length--;
+            }
+
+            return sb.ToString();
+        }
+#else
         private static string SanitizeHeaderValue(string input) {
             if (input == null) return null;
             if (input.IndexOf('\n') < 0) return input.Trim();
@@ -30,10 +69,12 @@ namespace Dalion.HttpMessageSigning.SigningString {
                 if (sb.Length > 0) {
                     sb.Append(' ');
                 }
+
                 sb.Append(line.Trim());
             }
 
             return sb.ToString();
         }
+#endif
     }
 }
